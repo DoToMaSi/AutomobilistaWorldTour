4b9ba79823b2e87420dbe366c2d76e55280c0169
Douglas Tomacheski de Abreu e Silva (Gestran) <douglas.silva@gestran.com.br>
2022-12-28 15:39:23 -0300

    - New Race Module

---
+++ src/app/modules/race/race.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CoreModule } from "src/app/core/core.module";
+import { SharedModule } from "src/app/shared/shared.module";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CoreModule,
+    SharedModule
+  ],
+})
+
+export class RaceModule { }
