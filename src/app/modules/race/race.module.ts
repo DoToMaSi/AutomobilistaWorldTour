f6b88021581488b3b710664b50c5096d5fc34685
Douglas Tomacheski de Abreu e Silva (Gestran) <douglas.silva@gestran.com.br>
2022-12-28 16:30:55 -0300

    - Improved Race Module

--- src/app/modules/race/race.module.ts
+++ src/app/modules/race/race.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CoreModule } from "src/app/core/core.module";
 import { SharedModule } from "src/app/shared/shared.module";
+import { RaceRouting } from "./race.routing";
 
 @NgModule({
   declarations: [],
   imports: [
     CoreModule,
-    SharedModule
+    SharedModule,
+    RaceRouting
   ],
 })
 
