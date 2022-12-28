b78fa326d5bd8d12bfccab5596bb82d98ff5bb7c
Douglas Tomacheski de Abreu e Silva (Gestran) <douglas.silva@gestran.com.br>
2022-12-28 16:26:07 -0300

    - New Garage Features.

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'awt-header-toolbar',
   templateUrl: 'header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class HeaderComponent implements OnInit {
+
+  @Input() showBackButton = false;
+  @Input() breadcrumbText = ' Return';
 
   constructor() { }
 
--- src/app/modules/garage/garage.module.ts
+++ src/app/modules/garage/garage.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from "@angular/core";
 import { CoreComponentsModule } from "src/app/core/components/core-components.module";
 import { CoreModule } from "src/app/core/core.module";
 import { SharedModule } from "src/app/shared/shared.module";
+import { CarsComponent } from "./cars/cars.component";
 import { GaragePage } from "./garage.page";
 import { GarageRouting } from "./garage.routing";
+import { StatsComponent } from "./stats/stats.component";
 
 @NgModule({
   declarations: [
-    GaragePage
+    GaragePage,
+    CarsComponent,
+    StatsComponent
   ],
   imports: [
     CoreModule,
