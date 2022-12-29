408b9e2088c6c818d8ece8add466655e80d3c58a
Douglas Tomacheski de Abreu e Silva <dotomasi@outlook.com>
2022-12-29 14:08:46 -0300

    - New Car Brands

--- src/app/shared/enums/vehicle/car-brand.enum.ts
+++ src/app/shared/enums/vehicle/car-brand.enum.ts
@@ -1,5 +1,29 @@
 export enum CarBrandEnum {
-  Toyota = 1
+  Toyota = 1,
+  Chevrolet = 2,
+  Reiza = 3,
+  Volkswagen = 4,
+  Porsche = 5,
+  MercedesBenz = 6,
+  BMW = 7,
+  McLaren = 8,
+  Brabham = 9,
+  Lotus = 10,
+  Metalmoro = 11,
+  Sigma = 12,
+  Puma = 13,
+  Reynard = 14,
+  Lola = 15,
+  Swift = 16,
+  Mitsubishi = 17,
+  Ginetta = 18,
+  Cadillac = 19,
+  MAN = 20,
+  Volvo = 21,
+  Iveco = 22,
+  Scania = 23,
+  Fiat = 24,
+  Caterham = 25
 }
 
 export const CarBrandEnumLabel = new Map<CarBrandEnum, string>([
